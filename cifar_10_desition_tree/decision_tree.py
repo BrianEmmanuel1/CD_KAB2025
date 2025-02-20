f710913a50e1c95e081ece70cd689d8d11011534
BrianEmmanuel1 <tprog.brian.hernandez@gmail.com>
2025-02-20 12:19:37 -0600

    depth and leaves metrics added

--- cifar_10_desition_tree/decision_tree.py
+++ cifar_10_desition_tree/decision_tree.py
@@ -16,7 +16,7 @@ def split_cifar_data():
     return x_train, y_train, x_test, y_test
 
 def regression_tree(x_train, y_train):
-    model = tree.DecisionTreeRegressor(max_depth=50, min_samples_split=5000)
+    model = tree.DecisionTreeRegressor(max_depth=1000, min_samples_split=500)
     model.fit(x_train, y_train)
     return model
 
@@ -24,7 +24,12 @@ def predict(model: tree.DecisionTreeRegressor, x_test):
     y_test = model.predict(x_test)
     return y_test
 
-def get_metrics(y_test, y_predict):
+def get_model_metrics(model: tree.DecisionTreeRegressor):
+    max_depth = model.get_depth()
+    total_leaves = model.get_n_leaves()
+    return max_depth, total_leaves
+
+def get_test_metrics(y_test, y_predict):
     mse = mean_squared_error(y_test, y_predict)
     r2s = r2_score(y_test, y_predict)
     return mse, r2s
