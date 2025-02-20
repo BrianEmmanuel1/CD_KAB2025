4b9d97aa91749b3fc1e5918314b106cb6ecb7fbf
BrianEmmanuel1 <tprog.brian.hernandez@gmail.com>
2025-02-20 12:07:34 -0600

    working model

---
+++ cifar_10_desition_tree/decision_tree.py
@@ -0,0 +1,30 @@
+from imports import Cifar10 as cif
+from sklearn import tree
+from sklearn.metrics import mean_squared_error, r2_score
+
+import numpy as np
+
+def split_cifar_data():
+    data = cif()
+
+    x_train = np.array(data.train_data.data).reshape(len(data.train_data), -1) / 255
+    y_train = np.array(data.train_data.targets) 
+    
+    x_test = np.array(data.test_data.data).reshape(len(data.test_data), -1) / 255
+    y_test = np.array(data.test_data.targets)
+    
+    return x_train, y_train, x_test, y_test
+
+def regression_tree(x_train, y_train):
+    model = tree.DecisionTreeRegressor(max_depth=50, min_samples_split=5000)
+    model.fit(x_train, y_train)
+    return model
+
+def predict(model: tree.DecisionTreeRegressor, x_test):
+    y_test = model.predict(x_test)
+    return y_test
+
+def get_metrics(y_test, y_predict):
+    mse = mean_squared_error(y_test, y_predict)
+    r2s = r2_score(y_test, y_predict)
+    return mse, r2s
